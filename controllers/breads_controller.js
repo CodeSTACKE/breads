69b017aeeedb9864981da3d476124adcbda4e8f9
Jyoti Das <48188772+CodeSTACKE@users.noreply.github.com>
2023-07-08 02:30:21 -0500

    update the code

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,13 +4,20 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('Index',
+  {
+    breads:Bread
+  }
+  )
+   // res.send(Bread)
 })
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    // res.send(Bread[req.params.arrayIndex])
-    res.json(Bread)
+    res.render('Show',{
+     bread: Bread[req.params.arrayIndex]
+    })    
+
   })
   
 
